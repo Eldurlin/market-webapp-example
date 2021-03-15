97fa446e3aa3a4c04f11c9c9c69800a7bf792ffd
eldurlin <adam.cakala@gmail.com>
2021-03-15 14:58:24 +0100

    Created forms for register page with WTForms.

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'e2d591d71c884e551ab61f48'
 db = SQLAlchemy(app)
 
 from market import routes
