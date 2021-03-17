f6feda4e95e2ce2cfcd1de7c2dcaa5c6ee22260c
eldurlin <adam.cakala@gmail.com>
2021-03-17 14:06:49 +0100

    Coded logout function and customized register/login redirect.

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app
 from market import db
 from flask import render_template, redirect, url_for, flash
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 
@@ -11,6 +11,7 @@ def home_page():
     return render_template('home.html')
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -22,6 +23,9 @@ def register_page():
         user_to_create = User(username=form.username.data, email_address=form.email_address.data, password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+
+        login_user(user_to_create)
+        flash(f'Account created successfully. You are now logged in as: {user_to_create.username}.', category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}:
         for err_msg in form.errors.values():
@@ -40,3 +44,9 @@ def login_page():
         else:
             flash('Username or password are incorrect. Please try again.', category='danger')
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash('You have been logged out.', category='info')
+    return redirect(url_for('home_page'))
